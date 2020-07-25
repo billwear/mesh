67496958e9ffaec1f79b334cf09487fcbe7b6675
billwear <bill.wear@canonical.com>
2020-07-25 17:54:19 -0500

    bot-push

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Just a little set of scripts that reproduce text versions of UI screens (like the Machine List, for example) on your terminal.   Also useful for CLI-centric documentation.
 
+* *machine-list* prints a text machine list, very much like the MAAS UI machine list.
+
 As usual -- YMMV, y'all.
--- machine-list.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Hostname	Power	Status	Owner		Pool	Zone	Fabric		Subnet"
-echo "		Type		Tags		Note	Spaces	VLAN		MAC Address"
-echo "---------------------------------------------------------------------------------------------------"
-maas admin machines read | jq -r '.[] | "\(.hostname)\t\(.power_state)\t\(.status_name)\t\(.owner)\t\t\(.pool.name)\t\(.zone.name)\t\(.boot_interface.vlan.fabric)\t\(.boot_interface.links[].subnet.name)\n\t\t\(.power_type)\t\t\(.tag_names)\t\t\t\(.boot_interface.vlan.name)\t\(.interface_set[].mac_address)"'
